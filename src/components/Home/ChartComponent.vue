d51cea2eab84fbbd24f4faad0cea884e4cd31640
mikoogla <78367511+mikoogla@users.noreply.github.com>
2023-01-23 10:52:30 +0100

    add chart + sample data

---
+++ src/components/Home/ChartComponent.vue
@@ -0,0 +1,76 @@
+<template>
+	<div>
+		<Line :data="data" :options="options" />
+	</div>
+</template>
+
+<script>
+import {
+	Chart as ChartJS,
+	CategoryScale,
+	LinearScale,
+	PointElement,
+	LineElement,
+	Title,
+	Tooltip,
+	Legend,
+} from "chart.js"
+import { Line } from "vue-chartjs"
+
+ChartJS.register(
+	CategoryScale,
+	LinearScale,
+	PointElement,
+	LineElement,
+	Title,
+	Tooltip,
+	Legend
+)
+
+export default {
+	name: "App",
+	props: {
+		labels: {
+			type: Array,
+			required: true,
+		},
+		chartName: {
+			type: String,
+			required: true,
+		},
+		stats: {
+			type: Array,
+			required: true,
+		},
+	},
+	components: {
+		Line,
+	},
+	data() {
+		return {
+			data: {
+				labels: this.labels,
+				datasets: [
+					{
+						label: this.chartName,
+						pointStyle: "circle",
+						backgroundColor: "#00ff6a",
+						borderColor: "#00ff6a",
+						data: this.stats,
+						tension: 0.3,
+					},
+				],
+			},
+			options: {
+				responsive: true,
+				maintainAspectRatio: true,
+				scales: {
+					y: {
+						beginAtZero: true,
+					},
+				},
+			},
+		}
+	},
+}
+</script>
--- src/components/Home/DashboardTiles/SummaryTile.vue
+++ src/components/Home/DashboardTiles/SummaryTile.vue
@@ -1,8 +1,40 @@
-<template><BackgroundTile radius="1rem">summary</BackgroundTile></template>
+<template>
+	<BackgroundTile radius="1rem" id="summary"
+		><div class="title w-100 ml-4 text-start">
+			<h1>Summary</h1>
+		</div>
+		<ChartComponent
+			:labels="labels"
+			:chartName="chartName"
+			:stats="stats"
+			class="chart"
+		/>
+	</BackgroundTile>
+</template>
 <script>
 import BackgroundTile from "../../UI/BackgroundTile.vue"
+import ChartComponent from "../ChartComponent.vue"
 export default {
+	data() {
+		return {
+			labels: ["January", "February", "March", "April", "May", "June", "July"],
+			chartName: "BTC Price",
+			stats: [6335, 5339, 8330, 8331, 5326, 5115, 4011],
+		}
+	},
 	name: "SummaryTile",
-	components: { BackgroundTile },
+	components: { BackgroundTile, ChartComponent },
 }
 </script>
+<style lang="scss" scoped>
+#summary {
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+}
+.chart {
+	width: 100%;
+	height: 100%;
+}
+</style>
